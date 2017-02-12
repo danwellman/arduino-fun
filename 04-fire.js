ada1e6de4251fb0078188e4819e78126e57f45ec
Dan Wellman <danwellman@hotmail.com>
2017-02-12 14:45:57 +0000

    servo tests

---
+++ 04-fire.js
@@ -0,0 +1,21 @@
+﻿var five = require('johnny-five')
+var board = new five.Board()
+
+board.on('ready', function () {
+    var pan = new five.Servo(9)
+    var tilt = new five.Servo(10)
+    var laser = new five.Led(11)
+
+    pan.sweep()
+    tilt.sweep()
+    laser.strobe(250)
+
+    board.wait(3000, function () {
+        laser.stop()
+        laser.off()
+        pan.stop()
+        pan.center()
+        tilt.stop()
+        tilt.center()
+    })
+})
